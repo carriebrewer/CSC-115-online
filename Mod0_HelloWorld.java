19e879baee7b86ac07ed81caf28ac1948ed3aa09
Carrie Brewer <42945276+carriebrewer@users.noreply.github.com>
2026-08-22 21:34:10 -0400

    Create Mod0_HelloWorld.java

---
+++ Mod0_HelloWorld.java
@@ -0,0 +1,11 @@
+// Carrie Brewer
+// 8/23/2026
+// This program writes a message to the screen.
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        
+        System.out.println("Hello, World!"); //writes to the screen
+
+    }
+}
