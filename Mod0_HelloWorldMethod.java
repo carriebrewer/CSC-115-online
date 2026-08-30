458d662b9eecb58821e156bd666de1a1dbe8a789
Carrie Brewer <42945276+carriebrewer@users.noreply.github.com>
2026-08-30 12:51:04 -0400

    Create Mod1_HelloWorldMethod.java

---
+++ Mod0_HelloWorldMethod.java
@@ -0,0 +1,22 @@
+// Carrie Brewer
+// 8/30/2026
+// This program writes a message to the screen.
+// Note: make this update in your original HelloWorld.java file
+
+public class HelloWorldMethod {
+    public static void main(String[] args) {
+        
+        printMessage();  // calls the method to print to the screen
+
+    }
+
+    // This method prints a message to the screen.
+    public static void printMessage() {
+         System.out.println("Hello, World!"); //writes to the screen
+
+    }
+}
+
+// 1. Write a comment and public static void... below the main method
+// 2. Copy the print statement from the main method into the new method
+// 3. Call the new method in the main method
